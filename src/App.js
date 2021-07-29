33d75786c93cee137d32fb3cc76381c680a4d662
Sam Clusker <sclusker@hotmail.com>
2021-07-29 21:53:16 +0100

    Successful swap from Ether to DAI.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
 import React from 'react';
-import connector from './components/connectors';
-import { Web3ReactProvider } from '@web3-react/core';
-import Web3 from 'web3';
 import Header from './components/Header';
-import Balance from './components/Balance';
 import ConnectMetaMask from './components/connectMetaMask';
-
+import SendEth from './components/sendEth'
 
 function App() {
   return (
     <div className="App">
-      <Web3ReactProvider 
-        connectors={connector}
-        libraryName={'web3.js'}
-        web3Api={Web3}
-      />
       <Header />
       <ConnectMetaMask />
-      <Balance />
+      <SendEth />
     </div>
   );
 }
--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
-import Web3 from 'web3';
+import React, { useState, useEffect } from 'react';
+//import Web3 from 'web3';
 
 const Balance = () => {
 
     const [accountInfo, setAccountInfo] = useState('');
     const { ethereum } = window;
-    
-    const getBalance = async () => new Promise ((resolve, reject) => {
-        let web3;
-        web3 = new Web3(ethereum.currentProvider)
-        web3.eth.getAccounts(
-            (error, accounts) => (error ? reject(error) : resolve(accounts) && setAccountInfo(JSON.stringify(accounts)) &&console.log(accounts))
+    const selectedAddress = ethereum.selectedAddress
+    setAccountInfo(selectedAddress)
+    /**const getBalance = async () => new Promise ((resolve, reject) => {
+        ethereum.eth.getAccounts(
+            (error, accounts) => (error ? reject(error) : resolve(accounts) && setAccountInfo(JSON.stringify(accounts)) && console.log(accounts))
         )
     })
+    
+    useEffect(() => {
+        getBalance();
+    },[]) **/
 
     return (
         <div>
